75af1659aa1c1f47752e755825bd787c501e7d29
vildfang <sebastianvildfang@gmail.com>
2026-04-07 10:19:49 +0200

    app

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import session from 'express-session'
+
+const app = express()
+
+
+// SETUP
+app.set('view engine', 'pug')
+
+// MIDDLEWARE
+app.use(express.static('assets'))
+// normal POST formulardata gjort tilgængelig
+// for at kunne modtage alm data i body skal express kunne læse dette
+app.use(express.urlencoded())
+// for at kunne modtage JSON i body skal express kunne læse dette
+app.use(express.json())
+
+// middleware der fanger resterende requests
+app.use((request, response, next)=>{
+    response.status(404).send('404 - Du tabte')
+})
+
+app.listen(8000, ()=>{
+    console.log("🚅 nu kører toget")
+})
