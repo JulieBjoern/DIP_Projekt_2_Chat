2517ca0eaab7b1329b478a23c9761378e7e38d3c
Mike Varming <eaa24mtv@students.eaaa.dk>
2026-04-09 06:57:43 +0200

    udkommenteret login så man ikke er låst

--- app.js
+++ app.js
@@ -26,12 +26,15 @@ app.use(session({
 app.use('/users', userRouter)
 
 app.get('/', (request, response)=>{
+    /*
     const isItAValidUser = request.session.isItAValidUser
     if (!isItAValidUser) {
         response.redirect('/users/login')
     } else {
         response.render('frontpage', {isItAValidUser})
     }
+    */
+     response.render('frontpage')
 })
 
 // middleware der fanger resterende requests
